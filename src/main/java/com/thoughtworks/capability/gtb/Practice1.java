a756d59b8e8dff81e42cea9baba4b4cd42bafeb1
JiajunLi229 <https://github.com/JiajunLi229>
2020-10-26 23:13:17 +0800

    feat: finish problem 1.

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +13,13 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    LocalDate labourDayThisYear =  LocalDate.of(date.getYear(), 5, 1);
+    LocalDate labourDayNextYear = labourDayThisYear.plusYears(1);
+
+    if (date.isBefore(labourDayThisYear)) {
+      return labourDayThisYear.getDayOfYear() - date.getDayOfYear();
+    } else {
+      return labourDayThisYear.lengthOfYear()- date.getDayOfYear()+labourDayNextYear.getDayOfYear();
+    }
   }
 }
